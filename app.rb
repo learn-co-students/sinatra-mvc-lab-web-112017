1dda9157363c981d2c1e93a1d1a6367b59a338ab
nspp99b <chromepanthers@gmail.com>
2017-12-12 13:25:05 -0500

    creatd erbs, controller actions,  and piglatinizer class

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    #creates instance of piglatinizer class using params[:user_input]
+    @piglatined = PigLatinizer.new.piglatinize(params[:user_phrase])
+    erb :piglatinize
+  end
+
 end
