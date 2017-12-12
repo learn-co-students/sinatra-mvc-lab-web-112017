1c9791712c7a556768dc2563b6cdb78744d27515
humzahchoudry <Humzah.choudry@gmail.com>
2017-12-12 11:47:14 -0500

    done

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    @text = params[:user_phrase]
+    @pl = PigLatinizer.new
+    @result = @pl.to_pig_latin(@text)
+    erb :result
+    # if text.split(' ').count > 1
+    #   @pl.to_pig_latin(text)
+    # else
+    #   @pl.piglatinize(text)
+    # end
+
+  end
 end
